b61176b041278659d3ffc5751b9a4e25e3a63c80
Hristo <hristo007@gmail.com>
2026-01-12 09:35:12 +0200

    Added seeding constants for admin, user and roles.

---
+++ ViralisClassroom/Viralis.Data/Constants/UserConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace Viralis.Data.Constants
+{
+    public class UserConstants
+    {
+        public const string Id = "67fccef3-e380-47c1-a59b-42e0b9e3f889";
+        public const string UserName = "seededUser@email.com";
+        public const string Email = "seededUser@email.com";
+        public const string PhoneNumber = "+676767676767";
+        public const string ConcurrencyStamp = "a1b2c3d4-e5f6-7890-abcd-ef1234567890";
+
+        public const string AdminId = "9016a84b-2b66-4ab6-9565-b348d4c3e18e";
+        public const string AdminUserName = "admin@admin.com";
+        public const string AdminEmail = "admin@admin.com";
+        public const string AdminPhoneNumber = "+000000000000";
+        public const string AdminConcurrencyStamp = "0f9e8d7c-6b5a-4321-0fed-cba987654321";
+
+        public const string DefaultPassword = "123456";
+    }
+}
